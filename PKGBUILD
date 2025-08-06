80c2da6d14330edb2ff3adbc956ffbd52dab04ef
Petexy / Linexy <piotr.petexiness@gmail.com>
2025-08-06 20:59:21 +0200

    250806-12

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: Petexy <https://github.com/Petexy>
 
 pkgname=linexin-desktop-presets
-pkgver=1.0.0.r
+pkgver=1.0.5.r
 pkgrel=1
 _currentdate=$(date +"%Y-%m-%d%H-%M-%S")
 pkgdesc='Smart Installer for Affinity suite for Linux'
@@ -23,6 +23,7 @@ package() {
    mkdir -p ${pkgdir}/usr/share/linexin/linexin-desktop
    mkdir -p ${pkgdir}/usr/share/icons
    mkdir -p ${pkgdir}/etc/skel/.local/share/linexin/linexin-desktop
+   mkdir -p ${pkgdir}/usr/share/gnome-shell/extensions
    cp -rf ${srcdir}/usr/bin/${pkgname} ${pkgdir}/usr/bin/${pkgname}
    cp -rf ${srcdir}/usr/share/applications ${pkgdir}/usr/share/
    cp -rf ${srcdir}/usr/share/icons ${pkgdir}/usr/share/
--- pkg/linexin-desktop-presets/etc/skel/.local/share/linexin/linexin-desktop/ubunexin.sh
+++ pkg/linexin-desktop-presets/etc/skel/.local/share/linexin/linexin-desktop/ubunexin.sh
@@ -4,4 +4,4 @@ dconf load /org/gnome/shell/extensions/dash-to-dock/ < $HOME/.local/share/linexi
 dconf load /org/gnome/shell/extensions/blur-my-shell/ < $HOME/.local/share/linexin/linexin-desktop/blur-ubunexin-linexin.ini
 gsettings set org.gnome.shell disabled-extensions "[]"
 gsettings set org.gnome.shell enabled-extensions "['accent-directories@taiwbi.com', 'blur-my-shell@aunetx', 'gtk4-ding@smedius.gitlab.com', 'dash-to-dock@micxgx.gmail.com', 'appindicatorsupport@rgcjonas.gmail.com', 'arch-update@RaphaelRochet', 'quick-settings-audio-panel@rayzeq.github.io']"
-gsettings set org.gnome.desktop.wm.preferences button-layout ':close,minimize,maximize'
+gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
