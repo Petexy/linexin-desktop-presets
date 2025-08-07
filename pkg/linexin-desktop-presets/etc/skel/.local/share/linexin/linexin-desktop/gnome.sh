1fc1122e4bc6d6245a04da1b07fd61062a64393a
Petexy / Linexy <piotr.petexiness@gmail.com>
2025-08-08 00:30:46 +0200

    250808-1

--- pkg/linexin-desktop-presets/etc/skel/.local/share/linexin/linexin-desktop/gnome.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-gsettings set org.gnome.shell enabled-extensions "[]"
-gsettings set org.gnome.shell disabled-extensions "[]"
-gsettings set org.gnome.desktop.wm.preferences button-layout ':close'
